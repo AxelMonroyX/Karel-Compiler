d04cd93cbb2422ef9a8eb7b098066bf9d4c76485
AxelMonroyX <xaxelmonroyx@gmail.com>
2015-05-05 22:59:16 -0700

    First commit

---
+++ src/karel/compilador/KarelCompilador.java
@@ -0,0 +1,21 @@
+
+package karel.compilador;
+
+/**
+ *
+ * @author Axel Monroy <xaxelmonroyx@gmail.com>
+ */
+public class KarelCompilador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Principal _Principal;
+        _Principal = new Principal();
+       _Principal.setVisible(true);
+    }
+    
+}
